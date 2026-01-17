ae336078546fe3642526fadc2911000703175cf3
Iulian Roman <iulian.roman@cognizant.com>
2026-01-17 14:26:19 +0200

    refactoring + directory structure

---
+++ js/app.js
@@ -0,0 +1,32 @@
+import { fetchCountries } from "./services/api.js";
+import * as Data from "./logic/dataManagement.js";
+import * as UI from "./ui/render.js";
+
+function handleSearch() {
+    const query = UI.getSearchValue();
+
+    if (!Data.isValidSearch(query)) {
+        if(query.length > 0) {
+            UI.displayError('Please enter at least 3 characters to search.');
+        } else {
+            UI.displayCountries([]); 
+        }
+        return;
+    }
+
+    const filteredData = Data.filterCountries(query);
+
+    UI.displayCountries(filteredData);
+}
+
+async function initApp() {
+    const data = await fetchCountries();
+    console.log(data);
+    if (data) {
+        Data.processCountries(data);
+    }
+
+    UI.setupInputListener(handleSearch);
+}
+
+initApp();
